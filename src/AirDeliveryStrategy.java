d2ce8f20efb5ff15dcc9c875f1c3aad9793d7320
ilyas <ilya_abramov_72@mail.ru>
2023-11-19 19:28:16 +0600

    first commit

--- src/AirDeliveryStrategy.java
+++ src/AirDeliveryStrategy.java
@@ -1,6 +1,6 @@
 public class AirDeliveryStrategy implements DeliveryStrategy {
     @Override
     public void deliver(Package pack) {
-        System.out.println("Delivering package by air to " + pack.getDestination());
+        System.out.println("Delivering package by air to  " + pack.getDestination());
     }
 }
